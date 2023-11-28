c83a66f7244ffb2fcad33dfb6cbd2367f7a02be8
rahmansodev <rahmansoftwaredev@gmail.com>
2023-11-28 16:17:16 +0800

    doc: add jsdoc

--- src/utils/tokenBan.util.ts
+++ src/utils/tokenBan.util.ts
@@ -1,6 +1,10 @@
 import { type UtilTokenBan } from '@interfaces/tokenBan.interface'
 import TokenBanRepo from '@repos/tokenBan.repo'
 
+/**
+ * Utility functions related to TokenBan.
+ * @type {UtilTokenBan}
+ */
 const TokenBanUtil: UtilTokenBan = {
   async isTokenBanned(token) {
     const tokenBan = await TokenBanRepo.findTokenBanByToken(token)
